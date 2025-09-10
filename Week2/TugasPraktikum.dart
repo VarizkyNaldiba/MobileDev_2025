ba84dd9ac21f18ca0ca3daddbde650bd01a914bb
varizky <varizkynr@gmail.com>
2025-09-10 20:41:01 +0700

    Update

--- Week2/TugasPraktikum.dart
+++ Week2/TugasPraktikum.dart
@@ -2,9 +2,9 @@ void main() {
   for (var i = 22; i > 0; i--) {
     print('My name was Variz and i am ${i - 1} years old');
 
-    String? nama; // Awalnya null
+    String? nama;   
     nama = "Budi";
-    print(nama.length); // Aman, hasilnya 4
+    print(nama.length);   
 
     late String deskripsi;
 
